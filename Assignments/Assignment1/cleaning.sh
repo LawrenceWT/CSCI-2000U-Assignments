c5b01dcf393f0150591852166d92d128b51ee0ff
Wesley <wtkl_109_108@hotmail.com>
2015-10-07 09:14:41 -0400

    Wesley Lawrence

---
+++ Assignments/Assignment1/cleaning.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for f in ~/Downloads/Assignments/Assignment1/cleaned_data/*
+do
+mv "$f" "$f.txt"
+done
---
+++ Assignments/Assignment1/simple.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a comment
+echo "The number of arguements is $#"
+echo "The arguements are $*"
+echo "The first arguement is $1"
+echo "My process number is $$"
+echo "Enter a number from the keyboard: "
+read number
+echo "The number you entered was $number"
